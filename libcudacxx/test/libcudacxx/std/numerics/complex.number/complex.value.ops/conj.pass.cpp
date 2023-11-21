ead7426bac69cbd6dc182f0cfc8caa09bb2dc102
Michał 'Griwes' Dominiak <griwes@griwes.info>
2023-11-21 23:35:46 +0100

    wip: small float complex

--- libcudacxx/test/libcudacxx/std/numerics/complex.number/complex.value.ops/conj.pass.cpp
+++ libcudacxx/test/libcudacxx/std/numerics/complex.number/complex.value.ops/conj.pass.cpp
@@ -43,6 +43,8 @@ int main(int, char**)
     test<double>();
 // CUDA treats long double as double
 //  test<long double>();
+    test<__half>();
+    test<__nv_bfloat16>();
 #if TEST_STD_VER > 11
     static_assert(test<float>(), "");
     static_assert(test<double>(), "");
