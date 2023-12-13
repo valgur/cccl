5ea0919e73c5d55480e0e08112c09db3f999cb9e
Michael Schellenberger Costa <miscco@nvidia.com>
2023-12-13 08:45:09 +0100

    Fix spelling of `_LIBCUDACXX_HAS_NO_CHAR8_T`

--- libcudacxx/include/cuda/std/detail/libcxx/include/__fwd/string.h
+++ libcudacxx/include/cuda/std/detail/libcxx/include/__fwd/string.h
@@ -31,7 +31,7 @@ struct _LIBCUDACXX_TEMPLATE_VIS char_traits;
 template <>
 struct char_traits<char>;
 
-#ifndef _LIBCUDACXX_NO_HAS_CHAR8_T
+#ifndef _LIBCUDACXX_HAS_NO_CHAR8_T
 template <>
 struct char_traits<char8_t>;
 #endif
@@ -58,7 +58,7 @@ using string = basic_string<char>;
 using wstring = basic_string<wchar_t>;
 #endif
 
-#ifndef _LIBCUDACXX_NO_HAS_CHAR8_T
+#ifndef _LIBCUDACXX_HAS_NO_CHAR8_T
 using u8string = basic_string<char8_t>;
 #endif
 
@@ -77,7 +77,7 @@ using string = basic_string<char>;
 using wstring = basic_string<wchar_t>;
 #  endif
 
-#  ifndef _LIBCUDACXX_NO_HAS_CHAR8_T
+#  ifndef _LIBCUDACXX_HAS_NO_CHAR8_T
 using u8string = basic_string<char8_t>;
 #  endif
 
@@ -94,7 +94,7 @@ class _LIBCUDACXX_PREFERRED_NAME(string)
 #ifndef _LIBCUDACXX_HAS_NO_WIDE_CHARACTERS
       _LIBCUDACXX_PREFERRED_NAME(wstring)
 #endif
-#ifndef _LIBCUDACXX_NO_HAS_CHAR8_T
+#ifndef _LIBCUDACXX_HAS_NO_CHAR8_T
       _LIBCUDACXX_PREFERRED_NAME(u8string)
 #endif
       _LIBCUDACXX_PREFERRED_NAME(u16string)
@@ -104,7 +104,7 @@ class _LIBCUDACXX_PREFERRED_NAME(string)
 #  ifndef _LIBCUDACXX_HAS_NO_WIDE_CHARACTERS
       _LIBCUDACXX_PREFERRED_NAME(pmr::wstring)
 #  endif
-#  ifndef _LIBCUDACXX_NO_HAS_CHAR8_T
+#  ifndef _LIBCUDACXX_HAS_NO_CHAR8_T
       _LIBCUDACXX_PREFERRED_NAME(pmr::u8string)
 #  endif
       _LIBCUDACXX_PREFERRED_NAME(pmr::u16string)
